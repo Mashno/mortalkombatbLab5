92f3db0653b4ab2130583ce9642e7e828a672ed9
Mashno <118561914+Mashno@users.noreply.github.com>
2025-05-23 15:44:26 +0300

    commit

---
+++ Java_laboratory_5/src/main/java/com/mycompany/java_laboratory_5/Java_laboratory_5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_laboratory_5;
+
+/**
+ *
+ * @author Владислав
+ */
+public class Java_laboratory_5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
